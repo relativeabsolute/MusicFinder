2b9604f2f90f127631190ebd7f1698944e591e8b
Johan Burke <jburke4126@gmail.com>
2018-09-18 02:16:45 -0400

    Added storage of current list of subreddits

--- js/musicfinder.index.jquery.js
+++ js/musicfinder.index.jquery.js
@@ -1,11 +1,37 @@
+let subreddits = [];
+
+function readSubreddits() {
+    let tempSubreddits = JSON.parse(window.localStorage.getItem('subreddits'));
+    console.log('subreddits:');
+    for (var index in tempSubreddits) {
+        console.log(tempSubreddits[index]);
+        addSubreddit(tempSubreddits[index]);
+    }
+}
+
+function addSubreddit(name) {
+    subreddits.push(name);
+    $('#subredditList').append("<a class='item'>" + name + "</a>");
+}
+
+function writeSubreddits() {
+    console.log('subreddits');
+    for (var index in subreddits) {
+        console.log(subreddits[index]);
+    }
+    window.localStorage.setItem('subreddits', JSON.stringify(subreddits));
+}
+
 $(function() {
+    readSubreddits();
+
     // TODO: use local storage
     $('#addSubredditButton').click(function(e) {
-        // TODO: verify that it is a valid subreddit
         let search = $('#searchSubreddits');
         let val = search.search('get value');
-        $('#subredditList').append("<a class='item'>" + val + "</a>");
+        addSubreddit(val);
         search.search('set value', '');
+        writeSubreddits();
     });
 
     // TODO: filter subreddits that have already been added
@@ -25,3 +51,4 @@ $(function() {
         minCharacters: 2
     });
 });
+
