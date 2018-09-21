d2ae6c03cda4b7a925d094e7a3b08d72a5d8728e
Johan Burke <jburke4126@gmail.com>
2018-09-21 03:04:10 -0400

    Added content player sidebar and youtube embed

--- js/musicfinder.index.jquery.js
+++ js/musicfinder.index.jquery.js
@@ -2,45 +2,57 @@ let subreddits = [];
 
 function readSubreddits() {
     let tempSubreddits = JSON.parse(window.localStorage.getItem('subreddits'));
-    for (const index in tempSubreddits) {
-        addSubreddit(tempSubreddits[index]);
-    }
+    $.each(tempSubreddits, function (index, item) {
+        addSubreddit(item);
+    });
+}
+
+function addSubredditHTML(name) {
+    const removeID = `remove${name}`;
+    const removeButton = `<button class='ui button' id='${removeID}'>Remove</button>`;
+    const menuItem = `<a class='item' id='${name}' data-html="${removeButton}">${name}</a>`;
+    $('#subredditList').append(menuItem);
+    $(`#subredditList > #${name}`).popup({hoverable: true});
 }
 
 function addSubreddit(name) {
     subreddits.push(name);
-    const removeID = "remove" + name;
-    const removeButton = `<button class='ui button' id='${removeID}'>Remove</button>`;
-    const menuItem = `<a class='item' id='${name}' data-html="${removeButton}">${name}</a>`;
-    $('#subredditList').append(menuItem);
-    $(`#subredditList > #${name}`).popup({hoverable:true});
+    // TODO: change popup to appear below item, and add subreddit info modal dialog
+    addSubredditHTML(name);
 }
 
 function writeSubreddits() {
     window.localStorage.setItem('subreddits', JSON.stringify(subreddits));
 }
 
-function subredditMenuItemHoverIn(e) {
-    // TODO: show popup for merging or removing reddits from list
-}
-
-function subredditMenuItemHoverOut(e) {
-
-}
-
 function loadSubredditData(subredditData) {
     // TODO: filter for only youtube and other streaming sites
-    $.each(subredditData.data.children, function(index, item) {
-        let newContent = "<div class='ui segment'><a href='" + item.data.url + "'>" + item.data.title + "</a></div>";
-        $('#contentPane').append(newContent);
-        //$('#contentPane').append("<div class='item'>" + )
+    let contentPane = $('#contentPane');
+    contentPane.empty();
+    let first = true;
+    $.each(subredditData.data.children, function (index, item) {
+        // TODO: setup media player interface
+        if (item.data.domain === 'youtu.be') {
+            if (first) {
+                // TODO: autoplay
+                first = false;
+                const youtubeID = item.data.url.match(/youtu.be\/(\w+)/)[1];
+                $('.ui.embed').embed({
+                    url : `https://www.youtube.com/embed/${youtubeID}`
+                });
+            }
+            const newContent = `<div class='ui segment'><a href='${item.data.url}'>${item.data.title}</a></div>`;
+            contentPane.append(newContent);
+        }
     });
+    // TODO: set up buttons on top menu bar to interact with content player
+    $('.sidebar').sidebar('toggle');
 }
 
 function subredditMenuItemClick(e) {
     e.preventDefault();
     let subredditName = e.target.id;
-    let targetURL = 'https://www.reddit.com/r/' + subredditName + '.json';
+    let targetURL = `https://www.reddit.com/r/${subredditName}.json`;
     // TODO: set loading indicator
     $.get(targetURL, loadSubredditData);
 }
@@ -54,7 +66,7 @@ function subredditRemoveClick(e) {
 }
 
 function addSubredditButtonHandlers() {
-    $('#addSubredditButton').click(function(e) {
+    $('#addSubredditButton').click(function (e) {
         let search = $('#searchSubreddits');
         let val = search.search('get value');
         addSubreddit(val);
@@ -64,15 +76,16 @@ function addSubredditButtonHandlers() {
 }
 
 function searchSubredditHandlers() {
-    // TODO: filter subreddits that have already been added
     $('#searchSubreddits').search({
         apiSettings: {
-            onResponse: function(redditResponse) {
+            onResponse: function (redditResponse) {
                 let response = {
                     results: []
                 };
-                $.each(redditResponse.names, function(index, item) {
-                    response.results.push({title: item})
+                $.each(redditResponse.names, function (index, item) {
+                    if (!subreddits.includes(item)) {
+                        response.results.push({title: item})
+                    }
                 });
                 return response
             },
@@ -84,37 +97,48 @@ function searchSubredditHandlers() {
 
 function updateSidebarButton() {
     const elements = ["<div id='toggleSidebarContent'><i class='angle left icon'></i>Hide Subreddits</a></div>",
-            "<div id='toggleSidebarContent'><i class='angle right icon'></i>Show Subreddits</div>"];
+        "<div id='toggleSidebarContent'><i class='angle right icon'></i>Show Subreddits</div>"];
     const sidebarVisible = $('#subredditList').sidebar('is visible');
-    $('#toggleSidebarContent').replaceWith(elements[+ sidebarVisible]);
+    $('#toggleSidebarContent').replaceWith(elements[+sidebarVisible]);
 }
 
-function toggleSidebarHandlers() {
-    $('#toggleSidebar').click(function() {
-        $('#subredditList').sidebar('toggle');
-    });
-}
-
+// event handlers for the menu across the top
 function topMenuHandlers() {
-    toggleSidebarHandlers();
+    $('#toggleSidebar').click(function () {
+        $('.sidebar').sidebar('toggle');
+    });
 
     $('#sortByDropdown').dropdown();
 }
 
 function subredditListHandlers() {
     let sidebar = $('#subredditList');
-    sidebar.sidebar({context: $('.bottom.segment'),
-        onVisible : updateSidebarButton,
-        onHide : updateSidebarButton,
-        dimPage : false
+    sidebar.sidebar({
+        context: $('.bottom.segment'),
+        onVisible: updateSidebarButton,
+        onHide: updateSidebarButton,
+        dimPage: false,
+        transition: 'overlay'
     });
     // we need to use a delegated event handler since the element being attached to needs to
-    // exist already (which must be the document
+    // exist already (which must be the document)
     $(document).on('click', '[id^=remove]', subredditRemoveClick);
     $(document).on('click', '#subredditList > .item', subredditMenuItemClick);
 }
 
-$(function() {
+function sidebarHandlers() {
+    subredditListHandlers();
+
+    $('#contentPlayer').sidebar({
+        context: $('.bottom.segment'),
+        dimPage: false,
+        transition: 'overlay'
+    });
+}
+
+// this gets called when the document is ready
+// attach all the event handlers and do whatever else needs to be done on startup
+$(function () {
     readSubreddits();
 
     addSubredditButtonHandlers();
@@ -123,6 +147,6 @@ $(function() {
 
     topMenuHandlers();
 
-    subredditListHandlers();
+    sidebarHandlers();
 });
 
--- main.js
+++ main.js
@@ -7,111 +7,111 @@ const redirectURI = 'http://musicfinder/auth_callback';
 const userAgentString = 'desktop:musicfinder:v0.1.0 (by /u/relative_absolute)';
 var stateString = '';
 
-    function getStateString(length) {
-        let result = '';
-        for (let i = 0; i < length; i++) result += alphabet.charAt(Math.floor(Math.random() * alphabet.length))
-        return result
-    }
+function getStateString(length) {
+    let result = '';
+    for (let i = 0; i < length; i++) result += alphabet.charAt(Math.floor(Math.random() * alphabet.length))
+    return result
+}
 
-    function getAccessToken(code, successCallback) {
-        console.log('Attempting to get access token');
-        if (code) {
-            console.log('Getting access token');
-            const https = require('https');
-            const request = https.request({
-                method: 'POST',
-                hostname: 'www.reddit.com',
-                path: '/api/v1/access_token',
-                //url: 'https://www.reddit.com/api/v1/access_token',
-                auth: `${clientID}:`,
-                headers: { 'User-Agent' : userAgentString }
-            });
-
-
-            const postData = querystring.stringify({
-                'grant_type' : 'authorization_code',
-                'code' : code,
-                'redirect_uri' : redirectURI
-            });
-            request.on('error', (error) => {
-                console.log('Error occurred');
-                console.log(error)
-            });
-            request.on('response', (response) => {
-                console.log('Response received');
-                response.on('data', (chunk) => {
-                    console.log(`Chunk received: ${chunk}`);
-                    successCallback(chunk)
-                });
-                response.on('error', (error) => {
-                    console.log(`ERROR: ${JSON.stringify(error)}`)
-                })
-            });
-            request.write(postData);
-            request.end()
-        }
-    }
-
-    function handleCallback(callbackUrl) {
-        const urlObject = new URL(callbackUrl);
-        if (urlObject.hostname !== 'musicfinder') {
-            return null
-        }
-        console.log('Handling oauth callback');
-        const error = urlObject.searchParams.get('error');
-        const code = urlObject.searchParams.get('code');
-        const stateCheck = urlObject.searchParams.get('state');
-        if (error) {
-            console.log(error);
-            return null
-        }
-        if (stateCheck !== stateString) {
-            console.log('Bad request');
-            return null
-        }
-        return code
-    }
-
-    function createSimple() {
-        let win = new BrowserWindow({width:800,height:600});
-
-        win.loadFile("index.html");
-        //win.webContents.toggleDevTools();
-
-        win.on('closed', () => {
-            win = null
+function getAccessToken(code, successCallback) {
+    console.log('Attempting to get access token');
+    if (code) {
+        console.log('Getting access token');
+        const https = require('https');
+        const request = https.request({
+            method: 'POST',
+            hostname: 'www.reddit.com',
+            path: '/api/v1/access_token',
+            //url: 'https://www.reddit.com/api/v1/access_token',
+            auth: `${clientID}:`,
+            headers: {'User-Agent': userAgentString}
         });
-    }
 
-    function createWindow() {
-        let win = new BrowserWindow({width: 800, height: 600, 'node-integration': false, 'web-security': false});
 
-        const stateLength = 8;
-
-        stateString = getStateString(stateLength);
-
-        const url = `https://www.reddit.com/api/v1/authorize?client_id=${clientID}&response_type=code&response_type=code&state=${stateString}&redirect_uri=${redirectURI}&duration=permanent&scope=read identity mysubreddits`;
-
-        win.loadURL(url);
-
-        //win.webContents.on('will-navigate', (event, newUrl) => {
-        //    getAccessToken(handleCallback(newUrl))
-        //})
-
-        win.webContents.on('did-get-redirect-request', (event, oldUrl, newUrl) => {
-            getAccessToken(handleCallback(newUrl), (chunk) => {
-                // TODO: handle the access token somehow
-                win.loadFile("index.html")
+        const postData = querystring.stringify({
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': redirectURI
+        });
+        request.on('error', (error) => {
+            console.log('Error occurred');
+            console.log(error)
+        });
+        request.on('response', (response) => {
+            console.log('Response received');
+            response.on('data', (chunk) => {
+                console.log(`Chunk received: ${chunk}`);
+                successCallback(chunk)
+            });
+            response.on('error', (error) => {
+                console.log(`ERROR: ${JSON.stringify(error)}`)
             })
         });
-
-        win.on('closed', () => {
-            win = null
-        })
+        request.write(postData);
+        request.end()
     }
+}
 
-    app.on('ready', createSimple);
+function handleCallback(callbackUrl) {
+    const urlObject = new URL(callbackUrl);
+    if (urlObject.hostname !== 'musicfinder') {
+        return null
+    }
+    console.log('Handling oauth callback');
+    const error = urlObject.searchParams.get('error');
+    const code = urlObject.searchParams.get('code');
+    const stateCheck = urlObject.searchParams.get('state');
+    if (error) {
+        console.log(error);
+        return null
+    }
+    if (stateCheck !== stateString) {
+        console.log('Bad request');
+        return null
+    }
+    return code
+}
 
-    app.on('window-all-closed', () => {
-        app.quit()
+function createSimple() {
+    let win = new BrowserWindow({width: 800, height: 600, webPreferences: {webSecurity: false}});
+
+    win.loadFile("index.html");
+    //win.webContents.toggleDevTools();
+
+    win.on('closed', () => {
+        win = null
     });
+}
+
+function createWindow() {
+    let win = new BrowserWindow({width: 800, height: 600, 'node-integration': false, 'web-security': false});
+
+    const stateLength = 8;
+
+    stateString = getStateString(stateLength);
+
+    const url = `https://www.reddit.com/api/v1/authorize?client_id=${clientID}&response_type=code&response_type=code&state=${stateString}&redirect_uri=${redirectURI}&duration=permanent&scope=read identity mysubreddits`;
+
+    win.loadURL(url);
+
+    //win.webContents.on('will-navigate', (event, newUrl) => {
+    //    getAccessToken(handleCallback(newUrl))
+    //})
+
+    win.webContents.on('did-get-redirect-request', (event, oldUrl, newUrl) => {
+        getAccessToken(handleCallback(newUrl), (chunk) => {
+            // TODO: handle the access token somehow
+            win.loadFile("index.html")
+        })
+    });
+
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+app.on('ready', createSimple);
+
+app.on('window-all-closed', () => {
+    app.quit()
+});
